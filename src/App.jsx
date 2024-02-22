0538975be651973659bdcc4ab4fec08bc95a4058
Andrei Harenkov <pevec1@yandex.ru>
2024-02-22 10:13:54 +0300

    Revert "update 2024-02-22 v0.2"
    This reverts commit cd6835d99e5194475c9b177781d37e03cea8ec92.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import dataForm from "./../data.json";
 function App() {
   
   const [dataList, setList] = useState()
-  const [data, setData] = useState({0:{
+  const [data, setData] = useState({
     date: new Date().toLocaleDateString(),
-    traveled: 2,
-  }});
+    traveled: 0,
+  });
     console.log(data);
  
   const onSubmit = (event) => {
@@ -17,27 +17,24 @@ function App() {
     const formData = new FormData(target);
 
     const data2 = Object.fromEntries(formData);
-        console.log(data2, data);
+        console.log(data2, dataForm);
         let dates = data2.date, traveled = data2.traveled;
-          if (dates === Array.from(data).find((dat)=> dat.date == dates)?.date){
-            Array.from(data).map((dat)=> {
+          if (dates === dataForm.find((dat)=> dat.date == dates)?.date){
+            dataForm.map((dat)=> {
             if (dat.date === dates) {dat.traveled += Number(traveled)}
             })}
-          else {Array.from(data).push({ date: dates, traveled: Number(traveled)})
+          else {dataForm.push({ date: dates, traveled: Number(traveled)})
           }
-          console.log(data);
+          console.log(dataForm);
   };
   const onChange = (event) => {
-    console.log(event);
-    if (event.target.name === "date") {
-           setData({0:{[event.target.name]:event.target.value,["traveled"]:0}});
-    }
-    if (event.target.name === "traveled") {
-           setData({0:{["date"]:"22.02.2024",[event.target.name]:event.target.value}});
-    }
-        }
+    const { target } = event;
+    console.log(target)
+    const { value } = target;
+    setData(value);
+  };
   const handleClick = (i) => {
-    Array.from(data).splice(i, 1);
+    dataForm.splice(i, 1);
   }
 
   return (
@@ -49,7 +46,7 @@ function App() {
             <input
               type="text"
               name="date"
-              value={data[0].date}
+              value={data.date}
               onChange={onChange}
             />
           </div>
@@ -58,7 +55,7 @@ function App() {
             <input
               type="text"
               name="traveled"
-              value={data[0].traveled}
+              value={data.traveled}
               onChange={onChange}
             />
           </div>
@@ -70,12 +67,9 @@ function App() {
       <div className="row">
         <div className="answer">
           <ul>
-            {Array.from(data).map((record, i) => (
+            {[...dataForm].map((record, i) => (
               <li key={i}>
-                <span>{record.date}</span> <span>{record.traveled}</span>{" "}
-                <button className="delete" onClick={handleClick(i)}>
-                  удалить
-                </button>
+                <span>{record.date}</span> <span>{record.traveled}</span> <button className="delete" onClick={handleClick(i)}>удалить</button>
               </li>
             ))}
           </ul>
